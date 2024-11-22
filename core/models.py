4a154de43e45eda44307f27847d12535c787f2da
facetablear <facetablear@gmail.com>
2024-11-22 22:19:43 +0300

    Add News model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=False)
