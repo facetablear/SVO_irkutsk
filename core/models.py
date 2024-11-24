1d2ea44c47ab6301616ea93b9f3786b3f0349649
facetablear <facetablear@gmail.com>
2024-11-24 20:29:31 +0300

    Add str names for model attributes

--- core/models.py
+++ core/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=False)
+    title = models.CharField(max_length=100, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True, null=True)
+    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Ноовость'
+        verbose_name_plural = 'Ноовости'
+        ordering = ['-created_at']
