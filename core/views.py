98611312cf42cbd1cd7356ca92fa2f10b3bc29aa
facetablear <facetablear@gmail.com>
2024-11-24 20:29:58 +0300

    Return data into template

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import News
+
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls home view.")
+    news = News.objects.all()
+    _context = {'news': news,
+                'title': 'Домашняя страница', }
+    return render(request, 'core/home.html', _context)
